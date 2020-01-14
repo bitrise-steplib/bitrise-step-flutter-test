157c9f5f67f4e8c34b6c2165b8858842c0f19e03
Etienne Cadic <ecadic@LFR031966.local>
2020-01-14 13:07:23 +0100

    fix(Step): re-adding additionnal parameters
     --> so that devs can use different paths for flutter tests.
    NB: --coverage option not available as we redirect tests reports with --machine.
    Maybe another test coverage Step should be provided ?

--- step.sh
+++ step.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # fail if any commands fails
+set -e
 # debug log
 
 export PATH="$PATH":"/usr/local/flutter/.pub-cache/bin"
@@ -8,11 +9,10 @@ export PATH="$PATH":"/usr/local/flutter/bin/cache/dart-sdk/bin"
 flutter pub get
 flutter pub global activate junitreport
 
-test_run_dir="$BITRISE_TEST_RESULT_DIR/result_dir_1"
+test_run_dir="$BITRISE_TEST_RESULT_DIR/flutter_tests_results"
 mkdir "$test_run_dir"
 
-flutter test --machine > test_results.jsonl
+echo "Running flutter test --machine $additional_params"
+flutter test --machine $additional_params > test_results.jsonl
 tojunit -i test_results.jsonl -o $test_run_dir/TEST-report.xml
 echo "{\"test-name\":\"$test_run_name\"}" >> "$test_run_dir/test-info.json"
-
-
