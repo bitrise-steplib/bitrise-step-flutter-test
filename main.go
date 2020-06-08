b0b26a3a66025ef10028c2bcf170be4be0770a43
Szabados Áron <aron.szabados@bitrise.io>
2020-06-08 13:49:15 +0200

    [TOOL-2241] Implemented test result generation in JUNIT format (#6)
    * implemented test result generation in JUNIT format
    
    * tests exported, commands piped using go
    
    * auto install junitreport
    
    * testresultexport uses master
    
    * PR suggestions
    
    * PR suggestions
    
    * PR suggestions

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"os/exec"
 
 	"github.com/bitrise-io/go-utils/command"
+	"github.com/bitrise-io/go-utils/errorutil"
 	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-io/testresultexport/testresultexport"
 	"github.com/bitrise-tools/go-steputils/stepconf"
 	shellquote "github.com/kballard/go-shellquote"
 )
@@ -13,6 +17,7 @@ import (
 type config struct {
 	AdditionalParams string `env:"additional_params"`
 	ProjectLocation  string `env:"project_location,dir"`
+	TestResultsDir   string `env:"bitrise_test_result_dir,dir"`
 }
 
 func failf(msg string, args ...interface{}) {
@@ -21,6 +26,8 @@ func failf(msg string, args ...interface{}) {
 }
 
 func main() {
+	const testName = "Flutter test results"
+	const testResultFileName = "./flutter_junit_test_results.xml"
 	var cfg config
 	if err := stepconf.Parse(&cfg); err != nil {
 		failf("Issue with input: %s", err)
@@ -35,16 +42,69 @@ func main() {
 	fmt.Println()
 	log.Infof("Running test")
 
-	testCmd := command.New("flutter", append([]string{"test"}, additionalParams...)...).
+	pr, pw := io.Pipe()
+
+	if _, err := exec.LookPath("tojunit"); err != nil {
+		log.Infof("Command `tojunit` not found, installing...")
+		junitInstallCmd := command.New("flutter", append([]string{"pub", "global", "activate", "junitreport"})...).
+			SetStdout(os.Stdout).
+			SetStderr(os.Stderr).
+			SetDir(cfg.ProjectLocation)
+
+		fmt.Println()
+		log.Donef(fmt.Sprintf("$ %s", junitInstallCmd.PrintableCommandArgs()))
+		fmt.Println()
+
+		if err := junitInstallCmd.Run(); err != nil {
+			if errorutil.IsExitStatusError(err) {
+				failf("Command `tojunit` failed to install, error: %s", err)
+			}
+			failf("Failed to run command `tojunit`, %s", err)
+		}
+	}
+
+	testCmd := exec.Command("flutter", append([]string{"test", "--machine"}, additionalParams...)...)
+	junitCmd := exec.Command("tojunit", append([]string{"--output", testResultFileName})...)
+
+	testCmdModel := command.NewWithCmd(testCmd).
+		SetStdout(pw).
+		SetStderr(os.Stderr).
+		SetDir(cfg.ProjectLocation)
+
+	junitCmdModel := command.NewWithCmd(junitCmd).
+		SetStdin(pr).
 		SetStdout(os.Stdout).
 		SetStderr(os.Stderr).
 		SetDir(cfg.ProjectLocation)
 
 	fmt.Println()
-	log.Donef("$ %s", testCmd.PrintableCommandArgs())
+	log.Donef("$ %s | %s", testCmdModel.PrintableCommandArgs(), junitCmdModel.PrintableCommandArgs())
 	fmt.Println()
 
-	if err := testCmd.Run(); err != nil {
+	if err := testCmd.Start(); err != nil {
 		failf("Running command failed, error: %s", err)
 	}
+
+	if err := junitCmd.Start(); err != nil {
+		failf("Converting test results to junit format failed, error: %s", err)
+	}
+
+	if err := testCmd.Wait(); err != nil {
+		failf("Completing test command failed, error: %s", err)
+	}
+
+	if err := pw.Close(); err != nil {
+		failf("Closing pipe failed, error: %s", err)
+	}
+
+	if err := junitCmd.Wait(); err != nil {
+		failf("Completing conversion command failed, error: %s", err)
+	}
+
+	exporter := testresultexport.NewExporter(cfg.TestResultsDir)
+	if err := exporter.ExportTest(testName, testResultFileName); err != nil {
+		failf("Failed to export test result: %s", err)
+	}
+
+	log.Infof("test results exported in junit format successfully")
 }
